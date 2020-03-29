0e401d31bda71819f9738ad01bea991268f51e4e
lamz <43467384+alamramdhani@users.noreply.github.com>
2020-03-29 13:14:21 +0700

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,29 @@
+<head>
+  <title>Aplikasi Dropship</title>
+  <link rel="shortcut icon" href="favicon.png">
+</head>
+  <div class="navbar navbar-default navbar-fixed-top">
+        <div class="navbar-header">
+          <a href="index.php" class="navbar-brand">Aplikasi Dropshiper</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php"><i class='glyphicon glyphicon-home'></i> Beranda</a></li>
+            <li class="dropdown">
+                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">Master Data <b class=" glyphicon glyphicon-chevron-down "></b></a>
+                   <ul class="dropdown-menu">
+                   <li><a href="dropshiper.php">Data Dropshiper</a></li>
+                   <li><a href="barang.php">Data Barang</a></li>
+                   </ul></li>
+            <li><a href="transaksi.php"><i class='glyphicon glyphicon-plus'></i> Transaksi</a></li>
+            <li><a href="laporan.php"><i class='glyphicon glyphicon-file'></i> Laporan</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
---
+++ include/js.php
@@ -0,0 +1,3 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../jasalert/docs/assets/sweetalert/sweetalert.min.js"></script>
